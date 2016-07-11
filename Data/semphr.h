62dc8de507af8f365eb5836bd9614c9d064953a1
Djones A. Boni <djboni@gmail.com>
2016-07-11 00:04:12 -0300

    semaphore: Implementation

---
+++ Data/semphr.h
@@ -0,0 +1,46 @@
+/*
+ Arduinutil Semaphore - Semaphore implementation in C
+
+
+ Copyright 2016 Djones A. Boni
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#ifndef __ARDUINUTIL_SEMPHR_H__
+#define __ARDUINUTIL_SEMPHR_H__
+
+#include "Arduinutil.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Semaphore_t {
+    Size_t Count;
+    Size_t Max;
+};
+
+void Semaphore_initbinary(struct Semaphore_t *o);
+void Semaphore_init(struct Semaphore_t *o, Size_t max);
+uint8_t Semaphore_lock(struct Semaphore_t *o);
+uint8_t Semaphore_unlock(struct Semaphore_t *o);
+Size_t Semaphore_getcount(const struct Semaphore_t *o);
+Size_t Semaphore_getmax(const struct Semaphore_t *o);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __ARDUINUTIL_SEMPHR_H__ */
