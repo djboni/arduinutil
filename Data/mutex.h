1e667cc74740c0b453a37a4837e6dbd0c35bea5d
Djones A. Boni <djboni@gmail.com>
2016-07-11 00:23:35 -0300

    mutex: Implementation

---
+++ Data/mutex.h
@@ -0,0 +1,43 @@
+/*
+ Arduinutil Mutex - Mutex implementation in C
+
+
+ Copyright 2016 Djones A. Boni
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#ifndef __ARDUINUTIL_MUTEX_H__
+#define __ARDUINUTIL_MUTEX_H__
+
+#include "Arduinutil.h"
+#include "Data/semphr.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Mutex_t {
+	struct Semaphore_t sem;
+};
+
+void Mutex_init(struct Mutex_t *o);
+uint8_t Mutex_lock(struct Mutex_t *o);
+uint8_t Mutex_unlock(struct Mutex_t *o);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __ARDUINUTIL_MUTEX_H__ */
