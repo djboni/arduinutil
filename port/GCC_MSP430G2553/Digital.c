4406bd441414bdfbbd8fe8badd5f81212a0464b3
Djones A. Boni <djboni@gmail.com>
2016-08-02 19:44:40 -0300

    digital: Smaller code for pin2*() functions

--- port/GCC_MSP430G2553/Digital.c
+++ port/GCC_MSP430G2553/Digital.c
@@ -31,158 +31,65 @@ static uint8_t pin2bit(uint8_t pin);
 
 static volatile uint8_t *pin2sel(uint8_t pin)
 {
-    volatile uint8_t *ret;
-
-    switch(pin) {
-    case 0U: ret = &P1SEL; break;
-    case 1U: ret = &P1SEL; break;
-    case 2U: ret = &P2SEL; break;
-    case 3U: ret = &P2SEL; break;
-    case 4U: ret = &P2SEL; break;
-    case 5U: ret = &P2SEL; break;
-    case 6U: ret = &P2SEL; break;
-    case 7U: ret = &P2SEL; break;
-    case 8U: ret = &P2SEL; break;
-    case 9U: ret = &P2SEL; break;
-    case 10U: ret = &P1SEL; break;
-    case 11U: ret = &P1SEL; break;
-    case 12U: ret = &P1SEL; break;
-    case 13U: ret = &P1SEL; break;
-    case 14U: ret = &P1SEL; break;
-    case 15U: ret = &P1SEL; break;
-    }
-
-    return ret;
+    if(pin < 2 || pin >= 8)
+        return &P1SEL;
+    else
+        return &P2SEL;
 }
 
 static volatile uint8_t *pin2dir(uint8_t pin)
 {
-    volatile uint8_t *ret;
-
-    switch(pin) {
-    case 0U: ret = &P1DIR; break;
-    case 1U: ret = &P1DIR; break;
-    case 2U: ret = &P2DIR; break;
-    case 3U: ret = &P2DIR; break;
-    case 4U: ret = &P2DIR; break;
-    case 5U: ret = &P2DIR; break;
-    case 6U: ret = &P2DIR; break;
-    case 7U: ret = &P2DIR; break;
-    case 8U: ret = &P2DIR; break;
-    case 9U: ret = &P2DIR; break;
-    case 10U: ret = &P1DIR; break;
-    case 11U: ret = &P1DIR; break;
-    case 12U: ret = &P1DIR; break;
-    case 13U: ret = &P1DIR; break;
-    case 14U: ret = &P1DIR; break;
-    case 15U: ret = &P1DIR; break;
-    }
-
-    return ret;
+    if(pin < 2 || pin >= 8)
+        return &P1DIR;
+    else
+        return &P2DIR;
 }
 
 static volatile uint8_t *pin2ren(uint8_t pin)
 {
-    volatile uint8_t *ret;
-
-    switch(pin) {
-    case 0U: ret = &P1REN; break;
-    case 1U: ret = &P1REN; break;
-    case 2U: ret = &P2REN; break;
-    case 3U: ret = &P2REN; break;
-    case 4U: ret = &P2REN; break;
-    case 5U: ret = &P2REN; break;
-    case 6U: ret = &P2REN; break;
-    case 7U: ret = &P2REN; break;
-    case 8U: ret = &P2REN; break;
-    case 9U: ret = &P2REN; break;
-    case 10U: ret = &P1REN; break;
-    case 11U: ret = &P1REN; break;
-    case 12U: ret = &P1REN; break;
-    case 13U: ret = &P1REN; break;
-    case 14U: ret = &P1REN; break;
-    case 15U: ret = &P1REN; break;
-    }
-
-    return ret;
+    if(pin < 2 || pin >= 8)
+        return &P1REN;
+    else
+        return &P2REN;
 }
 
 static volatile uint8_t *pin2out(uint8_t pin)
 {
-    volatile uint8_t *ret;
-
-    switch(pin) {
-    case 0U: ret = &P1OUT; break;
-    case 1U: ret = &P1OUT; break;
-    case 2U: ret = &P2OUT; break;
-    case 3U: ret = &P2OUT; break;
-    case 4U: ret = &P2OUT; break;
-    case 5U: ret = &P2OUT; break;
-    case 6U: ret = &P2OUT; break;
-    case 7U: ret = &P2OUT; break;
-    case 8U: ret = &P2OUT; break;
-    case 9U: ret = &P2OUT; break;
-    case 10U: ret = &P1OUT; break;
-    case 11U: ret = &P1OUT; break;
-    case 12U: ret = &P1OUT; break;
-    case 13U: ret = &P1OUT; break;
-    case 14U: ret = &P1OUT; break;
-    case 15U: ret = &P1OUT; break;
-    }
-
-    return ret;
+    if(pin < 2 || pin >= 8)
+        return &P1OUT;
+    else
+        return &P2OUT;
 }
 
 static const volatile uint8_t *pin2in(uint8_t pin)
 {
-    const volatile uint8_t *ret;
-
-    switch(pin) {
-    case 0U: ret = &P1IN; break;
-    case 1U: ret = &P1IN; break;
-    case 2U: ret = &P2IN; break;
-    case 3U: ret = &P2IN; break;
-    case 4U: ret = &P2IN; break;
-    case 5U: ret = &P2IN; break;
-    case 6U: ret = &P2IN; break;
-    case 7U: ret = &P2IN; break;
-    case 8U: ret = &P2IN; break;
-    case 9U: ret = &P2IN; break;
-    case 10U: ret = &P1IN; break;
-    case 11U: ret = &P1IN; break;
-    case 12U: ret = &P1IN; break;
-    case 13U: ret = &P1IN; break;
-    case 14U: ret = &P1IN; break;
-    case 15U: ret = &P1IN; break;
-    }
-
-    return ret;
+    if(pin < 2 || pin >= 8)
+        return &P1IN;
+    else
+        return &P2IN;
 }
 
 static uint8_t pin2bit(uint8_t pin)
 {
-    volatile uint8_t ret;
-
     switch(pin) {
-    case 0U: ret = BIT1; break;
-    case 1U: ret = BIT2; break;
-    case 2U: ret = BIT2; break;
-    case 3U: ret = BIT1; break;
-    case 4U: ret = BIT0; break;
-    case 5U: ret = BIT3; break;
-    case 6U: ret = BIT4; break;
-    case 7U: ret = BIT5; break;
-    case 8U: ret = BIT7; break;
-    case 9U: ret = BIT6; break;
-    case 10U: ret = BIT4; break;
-    case 11U: ret = BIT7; break;
-    case 12U: ret = BIT6; break;
-    case 13U: ret = BIT5; break;
-    case 14U: ret = BIT0; break;
-    case 15U: ret = BIT3; break;
+    case 0U: return BIT1;
+    case 1U: return BIT2;
+    case 2U: return BIT2;
+    case 3U: return BIT1;
+    case 4U: return BIT0;
+    case 5U: return BIT3;
+    case 6U: return BIT4;
+    case 7U: return BIT5;
+    case 8U: return BIT7;
+    case 9U: return BIT6;
+    case 10U: return BIT4;
+    case 11U: return BIT7;
+    case 12U: return BIT6;
+    case 13U: return BIT5;
+    case 14U: return BIT0;
+    case 15U: return BIT3;
+    default: return 0U;
     }
-
-    return ret;
 }
 
 /** Change pin configuration. The modes are INPUT, OUTPUT and INPUT_PULLUP. */
