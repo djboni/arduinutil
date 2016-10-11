djboni/arduinutil

commit 61fd32d006c2aab3488f28405a573138373ca080

timer: Use floating point conversion to us and ms

port/GCC_MSP430G2553/Timer.c
@@ -134,7 +134,7 @@ uint32_t timerCounts(void)
  Note: This function requires interrupts to be enabled. */
 void delay(uint32_t ms)
 {
-    delayCounts(TIMER_MS_TO_COUNT(ms));
+    return timerCounts() * ( 4.0e-3 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Stop execution for a given time in microseconds.
@@ -142,7 +142,7 @@ void delay(uint32_t ms)
  Note: This function requires interrupts to be enabled. */
 void delayMicroseconds(uint32_t us)
 {
-    delayCounts(TIMER_US_TO_COUNT(us));
+    return timerCounts() * ( 4.0 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Stop execution for a given number of timer counts.

port/GCC_ATmega2560/Timer.c
@@ -92,7 +92,7 @@ ISR(TIMER0_OVF_vect)
  Note: This function may return an outdated value if interrupts are disabled. */
 uint32_t millis(void)
 {
-    return TIMER_COUNT_TO_MS(timerCounts());
+    return timerCounts() * ( 4.0e-3 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Return the number of microseconds the timer is running.
@@ -100,7 +100,7 @@ uint32_t millis(void)
  Note: This function may return an outdated value if interrupts are disabled. */
 uint32_t micros(void)
 {
-    return TIMER_COUNT_TO_US(timerCounts());
+    return timerCounts() * ( 4.0 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Return the number of counts the timer had.

port/GCC_ATmega328P/Timer.c
@@ -92,7 +92,7 @@ ISR(TIMER0_OVF_vect)
  Note: This function may return an outdated value if interrupts are disabled. */
 uint32_t millis(void)
 {
-    return TIMER_COUNT_TO_MS(timerCounts());
+    return timerCounts() * ( 4.0e-3 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Return the number of microseconds the timer is running.
@@ -100,7 +100,7 @@ uint32_t millis(void)
  Note: This function may return an outdated value if interrupts are disabled. */
 uint32_t micros(void)
 {
-    return TIMER_COUNT_TO_US(timerCounts());
+    return timerCounts() * ( 4.0 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
 }
 
 /** Return the number of counts the timer had.

commit 4834d5b16eb3d4175e3b3e58e495ea587e0f5814

port: Fix delay on MSP430
Bug introduced in commit 61fd32d006c2aab3488f28405a573138373ca080.

port/GCC_MSP430G2553/Timer.c
@@ -134,7 +134,7 @@ uint32_t timerCounts(void)
  Note: This function requires interrupts to be enabled. */
 void delay(uint32_t ms)
 {
-    return timerCounts() * ( 4.0e-3 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
+    delayCounts(TIMER_MS_TO_COUNT(ms));
 }
 
 /** Stop execution for a given time in microseconds.
@@ -142,7 +142,7 @@ void delay(uint32_t ms)
  Note: This function requires interrupts to be enabled. */
 void delayMicroseconds(uint32_t us)
 {
-    return timerCounts() * ( 4.0 * (TIMER_PRESCALER / 64.0) / (F_CPU / 16.0e6) );
+    delayCounts(TIMER_US_TO_COUNT(us));
 }
 
 /** Stop execution for a given number of timer counts.